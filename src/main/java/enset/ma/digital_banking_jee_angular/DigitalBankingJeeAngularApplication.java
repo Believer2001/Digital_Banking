e6345709714b4b95ee3c656d25c4ac99bdb7e19b
Believer2001 <tsehbenoit2001@gmail.com>
2025-04-28 09:50:46 +0100

    initilisation du projet

---
+++ src/main/java/enset/ma/digital_banking_jee_angular/DigitalBankingJeeAngularApplication.java
@@ -0,0 +1,13 @@
+package enset.ma.digital_banking_jee_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingJeeAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingJeeAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/enset/ma/digital_banking_jee_angular/DigitalBankingJeeAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.ma.digital_banking_jee_angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingJeeAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
